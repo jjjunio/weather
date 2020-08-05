68add966e9fac7ba2589207d45f864cae15e0505
jjjunio <jjjunio@gmail.com>
2020-08-05 13:23:38 -0400

    created config.js and added api key

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 window.addEventListener('load', () => {
-  let long;
+  var apiKey = config.API_KEY;
+  let lon;
   let lat;
+  
 
   if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
-      long = position.coords.longitude;
-      lat = position.coords.latitude;
-      
+      lon = position.coords.longitude.toFixed(2);
+      lat = position.coords.latitude.toFixed(2);
+      console.log("Latitude:", lat);
+      console.log("Longitude:", lon);
+
+      const api = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+
     })
   } else {
     h1.textContent = "please enable your geolocation"
